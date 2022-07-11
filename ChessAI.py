aa595342a7dce29fe7eefaa50fb8dac21f0b5d41
tangobones <h.mello.chaves@gmail.com>
2022-07-10 22:09:09 -0300

    NegaMax algo implementation

--- ChessAI.py
+++ ChessAI.py
@@ -17,46 +17,12 @@ def findRandomMove(validMoves):
     """
     return validMoves[random.randint(0, len(validMoves)-1)]
 
-
-# iterative min max with depth of 2
-def findBestMove(gs, validMoves):
-    turnMultiplier = 1 if gs.whiteToMove else -1 
-    oppMinMaxScore = CHECKMATE
-    bestPlayerMove = None
-    random.shuffle(validMoves)
-
-    for playerMove in validMoves:
-        gs.makeMove(playerMove)
-        oppMoves = gs.getValidMoves()
-        if gs.checkMate:
-            oppMaxScore = -CHECKMATE
-        elif gs.staleMate:
-            oppMaxScore = STALEMATE
-        else:
-            oppMaxScore = -CHECKMATE        
-            for oppMove in oppMoves:
-                gs.makeMove(oppMove)
-                gs.getValidMoves()
-                if gs.checkMate:
-                    score = CHECKMATE
-                elif gs.staleMate:
-                    score = STALEMATE
-                else:
-                    score = -turnMultiplier * scoreMaterial(gs.board)
-                if score > oppMaxScore:
-                    oppMaxScore = score
-                gs.undoMove()
-        if oppMaxScore < oppMinMaxScore:
-            oppMinMaxScore = oppMaxScore
-            bestPlayerMove = playerMove
-        gs.undoMove()
-    return bestPlayerMove
-
 #helper method to make the first recursive call
-def findBestMoveMinMax(gs, validMoves):
+def findBestMove(gs, validMoves):
     global nextMove
     nextMove = None
-    findMoveMinMax(gs,validMoves,DEPTH,gs.whiteToMove)
+    #findMoveNegaMax(gs,validMoves,DEPTH,gs.whiteToMove)
+    findMoveNegaMax(gs, validMoves, DEPTH, 1 if gs.whiteToMove else -1)
     return nextMove
 
 def findMoveMinMax(gs, validMoves, depth, whiteToMove):
@@ -90,6 +56,23 @@ def findMoveMinMax(gs, validMoves, depth, whiteToMove):
             gs.undoMove()
         return minScore
 
+def findMoveNegaMax(gs, validMoves, depth, turnMultiplier):
+    global nextMove
+    if depth == 0:
+        return turnMultiplier * scoreBoard(gs)
+    
+    maxScore = -CHECKMATE
+    for move in validMoves:
+        gs.makeMove(move)
+        nextMoves = gs.getValidMoves()
+        score = -findMoveNegaMax(gs, nextMoves, depth - 1, -turnMultiplier)
+        if score > maxScore:
+            maxScore = score
+            if depth == DEPTH:
+                nextMove = move
+        gs.undoMove()
+    return maxScore
+
 # A positive score is good for white a negative score is good for black
 def scoreBoard(gs):
     if gs.checkMate:
@@ -99,8 +82,6 @@ def scoreBoard(gs):
             return CHECKMATE #white wins
     elif gs.staleMate:
         return STALEMATE
-
-
 
     score = 0
     for row in gs.board:
--- ChessMain.py
+++ ChessMain.py
@@ -79,7 +79,7 @@ def main():
                 
                 #AI move finder logic
                 if not gameOver and not isHumanTurn:
-                    AIMove = ChessAI.findBestMoveMinMax(gs, validMoves)
+                    AIMove = ChessAI.findBestMove(gs, validMoves)
                     if AIMove is None:
                         AIMove = ChessAI.findRandomMove(validMoves)
                     gs.makeMove(AIMove)
